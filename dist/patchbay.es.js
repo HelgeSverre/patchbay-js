fba2773d796661861a488b198357e9318666ce97
Helge Sverre <helge.sverre@gmail.com>
2024-09-24 22:37:47 +0200

    build

--- dist/patchbay.es.js
+++ dist/patchbay.es.js
@@ -1,14 +1,13 @@
 var f = Object.defineProperty;
-var u = (l, t, e) => (t in l ? f(l, t, { enumerable: !0, configurable: !0, writable: !0, value: e }) : (l[t] = e));
+var u = (l, t, e) => t in l ? f(l, t, { enumerable: !0, configurable: !0, writable: !0, value: e }) : l[t] = e;
 var g = (l, t, e) => (u(l, typeof t != "symbol" ? t + "" : t, e), e);
 class d {
   constructor(t, e) {
-    (this.x = t), (this.y = e), (this.oldX = t), (this.oldY = e);
+    this.x = t, this.y = e, this.oldX = t, this.oldY = e;
   }
   update(t) {
-    const e = this.x - this.oldX,
-      s = this.y - this.oldY + t;
-    (this.oldX = this.x), (this.oldY = this.y), (this.x += e), (this.y += s);
+    const e = this.x - this.oldX, s = this.y - this.oldY + t;
+    this.oldX = this.x, this.oldY = this.y, this.x += e, this.y += s;
   }
 }
 class p {
@@ -26,39 +25,24 @@ class p {
       slack: 1.1,
       snapRadius: 100,
       snapTargetSelector: ".cable-connector",
-      draggable: !0,
+      draggable: !0
     });
-    (this.config = { ...this.config, ...t }),
-      (this.start = this.config.start),
-      (this.end = this.config.end),
-      (this.isDragging = !1),
-      (this.draggedEnd = null),
-      (this.element = this.createMainElement()),
-      (this.points = this.initializePoints()),
-      (this.segmentElements = this.createSegmentElements()),
-      this.config.draggable && (this.setupDragHandles(), this.setupDragEvents());
+    this.config = { ...this.config, ...t }, this.start = this.config.start, this.end = this.config.end, this.isDragging = !1, this.draggedEnd = null, this.element = this.createMainElement(), this.points = this.initializePoints(), this.segmentElements = this.createSegmentElements(), this.config.draggable && (this.setupDragHandles(), this.setupDragEvents());
   }
   createMainElement() {
     const t = document.createElement("div");
-    return (
-      t.classList.add("cable-container"),
-      (t.style.position = "fixed"),
-      (t.style.top = "0"),
-      (t.style.left = "0"),
-      (t.style.pointerEvents = "none"),
-      t
-    );
+    return t.classList.add("cable-container"), t.style.position = "fixed", t.style.top = "0", t.style.left = "0", t.style.pointerEvents = "none", t;
   }
   initializePoints() {
-    const t = this.getElementCenter(this.start),
-      e = this.getElementCenter(this.end),
-      s = [new d(t.x, t.y)],
-      n = e.x - t.x,
-      i = e.y - t.y,
-      o = (Math.sqrt(n * n + i * i) * this.config.slack) / (this.config.segments - 1);
+    const t = this.getElementCenter(this.start), e = this.getElementCenter(this.end), s = [new d(t.x, t.y)], n = e.x - t.x, i = e.y - t.y, o = Math.sqrt(n * n + i * i) * this.config.slack / (this.config.segments - 1);
     for (let r = 1; r < this.config.segments - 1; r++) {
       const h = r / (this.config.segments - 1);
-      s.push(new d(t.x + n * h + (Math.random() - 0.5) * o, t.y + i * h + (Math.random() - 0.5) * o));
+      s.push(
+        new d(
+          t.x + n * h + (Math.random() - 0.5) * o,
+          t.y + i * h + (Math.random() - 0.5) * o
+        )
+      );
     }
     return s.push(new d(e.x, e.y)), s;
   }
@@ -69,153 +53,102 @@ class p {
    * @returns {{x, y}|null|{x: number, y: number}}
    */
   getElementCenter(t) {
-    if (t instanceof d || (t.hasOwnProperty("x") && t.hasOwnProperty("y")))
+    if (t instanceof d || t.hasOwnProperty("x") && t.hasOwnProperty("y"))
       return {
         x: t.x,
-        y: t.y,
+        y: t.y
       };
     if (t instanceof HTMLElement) {
       const { height: e, left: s, top: n, width: i } = t.getBoundingClientRect();
       return {
         x: s + i / 2,
-        y: n + e / 2,
+        y: n + e / 2
       };
     }
     return null;
   }
   setupDragHandles() {
-    (this.startHandle = this.createDragHandle()),
-      (this.endHandle = this.createDragHandle()),
-      this.element.appendChild(this.startHandle),
-      this.element.appendChild(this.endHandle);
+    this.startHandle = this.createDragHandle(), this.endHandle = this.createDragHandle(), this.element.appendChild(this.startHandle), this.element.appendChild(this.endHandle);
   }
   setupDragEvents() {
-    this.startHandle.addEventListener("mousedown", (t) => this.startDragging(t, "start")),
-      this.endHandle.addEventListener("mousedown", (t) => this.startDragging(t, "end")),
-      document.addEventListener("mousemove", (t) => this.drag(t)),
-      document.addEventListener("mouseup", (t) => this.stopDragging(t));
+    this.startHandle.addEventListener("mousedown", (t) => this.startDragging(t, "start")), this.endHandle.addEventListener("mousedown", (t) => this.startDragging(t, "end")), document.addEventListener("mousemove", (t) => this.drag(t)), document.addEventListener("mouseup", (t) => this.stopDragging(t));
   }
   createDragHandle() {
     const t = document.createElement("div");
-    return (
-      t.classList.add("cable-drag-handle"),
-      (t.style.width = `${this.config.dragHandleSize}px`),
-      (t.style.height = `${this.config.dragHandleSize}px`),
-      (t.style.position = "absolute"),
-      (t.style.borderRadius = "50%"),
-      (t.style.backgroundColor = "transparent"),
-      (t.style.pointerEvents = "all"),
-      (t.style.cursor = "move"),
-      (t.style.zIndex = "9999"),
-      t
-    );
+    return t.classList.add("cable-drag-handle"), t.style.width = `${this.config.dragHandleSize}px`, t.style.height = `${this.config.dragHandleSize}px`, t.style.position = "absolute", t.style.borderRadius = "50%", t.style.backgroundColor = "transparent", t.style.pointerEvents = "all", t.style.cursor = "move", t.style.zIndex = "9999", t;
   }
   startDragging(t, e) {
-    (this.isDragging = !0), (this.draggedEnd = e), e === "start" ? (this.start = null) : (this.end = null);
+    this.isDragging = !0, this.draggedEnd = e, e === "start" ? this.start = null : this.end = null;
   }
   drag(t) {
-    if (!this.isDragging) return;
+    if (!this.isDragging)
+      return;
     const { clientX: e, clientY: s } = t;
-    this.draggedEnd === "start"
-      ? ((this.points[0].x = e), (this.points[0].y = s))
-      : ((this.points[this.points.length - 1].x = e), (this.points[this.points.length - 1].y = s)),
-      this.update();
+    this.draggedEnd === "start" ? (this.points[0].x = e, this.points[0].y = s) : (this.points[this.points.length - 1].x = e, this.points[this.points.length - 1].y = s), this.update();
   }
   stopDragging() {
-    if (!this.isDragging) return;
-    const t = this.draggedEnd === "start" ? this.points[0] : this.points[this.points.length - 1],
-      e = this.findSnapTarget(t.x, t.y);
+    if (!this.isDragging)
+      return;
+    const t = this.draggedEnd === "start" ? this.points[0] : this.points[this.points.length - 1], e = this.findSnapTarget(t.x, t.y);
     if (e) {
-      this.draggedEnd === "start" ? (this.start = e) : (this.end = e);
+      this.draggedEnd === "start" ? this.start = e : this.end = e;
       const s = this.getElementCenter(e);
-      (t.x = s.x), (t.y = s.y);
+      t.x = s.x, t.y = s.y;
     }
-    (this.isDragging = !1), (this.draggedEnd = null), this.update();
+    this.isDragging = !1, this.draggedEnd = null, this.update();
   }
   findSnapTarget(t, e) {
     const s = document.querySelectorAll(this.config.snapTargetSelector);
-    let n = null,
-      i = 1 / 0;
+    let n = null, i = 1 / 0;
     for (let a of s) {
-      const o = a.getBoundingClientRect(),
-        r = o.left + o.width / 2,
-        h = o.top + o.height / 2,
-        c = Math.sqrt((t - r) ** 2 + (e - h) ** 2);
-      c < i && ((i = c), (n = a));
+      const o = a.getBoundingClientRect(), r = o.left + o.width / 2, h = o.top + o.height / 2, c = Math.sqrt((t - r) ** 2 + (e - h) ** 2);
+      c < i && (i = c, n = a);
     }
     return i <= this.config.snapRadius ? n : null;
   }
   createSegmentElements() {
     return this.points.slice(0, -1).map((t, e) => {
       const s = document.createElement("div");
-      return (
-        s.classList.add("cable-segment"),
-        (s.style.position = "absolute"),
-        (s.style.background = this.config.color),
-        (s.style.height = this.config.lineThickness + "px"),
-        (s.style.transformOrigin = "0 50%"),
-        this.element.appendChild(s),
-        s
-      );
+      return s.classList.add("cable-segment"), s.style.position = "absolute", s.style.background = this.config.color, s.style.height = this.config.lineThickness + "px", s.style.transformOrigin = "0 50%", this.element.appendChild(s), s;
     });
   }
   update() {
     if (this.start) {
       const t = this.getElementCenter(this.start);
-      (this.points[0].x = t.x), (this.points[0].y = t.y);
+      this.points[0].x = t.x, this.points[0].y = t.y;
     }
     if (this.end) {
       const t = this.getElementCenter(this.end);
-      (this.points[this.points.length - 1].x = t.x), (this.points[this.points.length - 1].y = t.y);
+      this.points[this.points.length - 1].x = t.x, this.points[this.points.length - 1].y = t.y;
     }
-    for (let t = 1; t < this.points.length - 1; t++) this.points[t].y += this.config.gravity;
-    for (let t = 0; t < this.config.iterations; t++) this.solveConstraints();
+    for (let t = 1; t < this.points.length - 1; t++)
+      this.points[t].y += this.config.gravity;
+    for (let t = 0; t < this.config.iterations; t++)
+      this.solveConstraints();
     this.render();
   }
   solveConstraints() {
     const t = this.calculateSegmentLength();
     for (let e = 0; e < this.points.length - 1; e++) {
-      const s = this.points[e],
-        n = this.points[e + 1],
-        i = n.x - s.x,
-        a = n.y - s.y,
-        o = Math.sqrt(i * i + a * a),
-        r = (t - o) / o,
-        h = i * 0.5 * r,
-        c = a * 0.5 * r;
-      e > 0 && ((s.x -= h), (s.y -= c)), e < this.points.length - 2 && ((n.x += h), (n.y += c));
+      const s = this.points[e], n = this.points[e + 1], i = n.x - s.x, a = n.y - s.y, o = Math.sqrt(i * i + a * a), r = (t - o) / o, h = i * 0.5 * r, c = a * 0.5 * r;
+      e > 0 && (s.x -= h, s.y -= c), e < this.points.length - 2 && (n.x += h, n.y += c);
     }
   }
   calculateSegmentLength() {
     return this.calculateTotalLength() / (this.config.segments - 1);
   }
   calculateTotalLength() {
-    const t = this.points[0],
-      e = this.points[this.points.length - 1],
-      s = e.x - t.x,
-      n = e.y - t.y;
+    const t = this.points[0], e = this.points[this.points.length - 1], s = e.x - t.x, n = e.y - t.y;
     return Math.sqrt(s * s + n * n) * this.config.slack;
   }
   render() {
     this.segmentElements.forEach((t, e) => {
-      const s = this.points[e],
-        n = this.points[e + 1],
-        i = n.x - s.x,
-        a = n.y - s.y,
-        o = Math.sqrt(i * i + a * a),
-        r = Math.atan2(a, i);
-      (t.style.width = `${o}px`),
-        (t.style.left = `${s.x}px`),
-        (t.style.top = `${s.y}px`),
-        (t.style.transform = `rotate(${r}rad)`);
-    }),
-      this.config.draggable &&
-        (this.updateDragHandlePosition(this.startHandle, this.points[0]),
-        this.updateDragHandlePosition(this.endHandle, this.points[this.points.length - 1]));
+      const s = this.points[e], n = this.points[e + 1], i = n.x - s.x, a = n.y - s.y, o = Math.sqrt(i * i + a * a), r = Math.atan2(a, i);
+      t.style.width = `${o}px`, t.style.left = `${s.x}px`, t.style.top = `${s.y}px`, t.style.transform = `rotate(${r}rad)`;
+    }), this.config.draggable && (this.updateDragHandlePosition(this.startHandle, this.points[0]), this.updateDragHandlePosition(this.endHandle, this.points[this.points.length - 1]));
   }
   updateDragHandlePosition(t, e) {
-    (t.style.left = `${e.x - this.config.dragHandleSize / 2}px`),
-      (t.style.top = `${e.y - this.config.dragHandleSize / 2}px`);
+    t.style.left = `${e.x - this.config.dragHandleSize / 2}px`, t.style.top = `${e.y - this.config.dragHandleSize / 2}px`;
   }
 }
 class m {
@@ -237,27 +170,15 @@ class m {
       dragHandleSize: 20,
       lineThickness: 2,
       snapElementSelector: ".cable-connector",
-      zIndex: 9999,
+      zIndex: 9999
     });
-    if (((this.config = { ...this.config, ...t }), (this.cables = []), !this.config.container))
+    if (this.config = { ...this.config, ...t }, this.cables = [], !this.config.container)
       throw new Error("No container element provided for patchbay");
-    (this.root = this.createRootElement()), this.config.container.appendChild(this.root);
+    this.root = this.createRootElement(), this.config.container.appendChild(this.root);
   }
   createRootElement() {
     const t = document.createElement("div");
-    return (
-      t.classList.add("patchbay-container"),
-      (t.style.position = "fixed"),
-      (t.style.top = "0"),
-      (t.style.bottom = "0"),
-      (t.style.left = "0"),
-      (t.style.right = "0"),
-      (t.style.width = "100%"),
-      (t.style.height = "100%"),
-      (t.style.zIndex = this.config.zIndex || 9999),
-      (t.style.pointerEvents = "none"),
-      t
-    );
+    return t.classList.add("patchbay-container"), t.style.position = "fixed", t.style.top = "0", t.style.bottom = "0", t.style.left = "0", t.style.right = "0", t.style.width = "100%", t.style.height = "100%", t.style.zIndex = this.config.zIndex || 9999, t.style.pointerEvents = "none", t;
   }
   /**
    * Create a new cable and add it to the patchbay.
@@ -273,7 +194,7 @@ class m {
     const i = new p({
       start: t,
       end: e,
-      ...n,
+      ...n
     });
     return this.cables.push(i), this.root.appendChild(i.element), i;
   }
@@ -289,7 +210,7 @@ class m {
     const i = new p({
       start: t,
       end: e,
-      ...n,
+      ...n
     });
     return this.cables.push(i), this.root.appendChild(i.element), i;
   }
@@ -299,7 +220,7 @@ class m {
     const n = new p({
       start: t,
       end: t,
-      ...s,
+      ...s
     });
     return n.startDragging("end"), n.update(), this.cables.push(n), this.root.appendChild(n.element), n;
   }
@@ -315,13 +236,14 @@ class m {
    * Update the physics simulation for all cables.
    */
   update() {
-    for (let t of this.cables) t.update();
+    for (let t of this.cables)
+      t.update();
   }
   /**
    * Start the animation loop.
    */
   start() {
-    (this.isRunning = !0), this.loop();
+    this.isRunning = !0, this.loop();
   }
   /**
    * Animation loop for physics simulation.
@@ -333,8 +255,12 @@ class m {
    * Stop the animation loop.
    */
   stop() {
-    (this.isRunning = !1), cancelAnimationFrame(this.loop);
+    this.isRunning = !1, cancelAnimationFrame(this.loop);
   }
 }
-export { p as Cable, m as Patchbay, d as Point };
+export {
+  p as Cable,
+  m as Patchbay,
+  d as Point
+};
 //# sourceMappingURL=patchbay.es.js.map
